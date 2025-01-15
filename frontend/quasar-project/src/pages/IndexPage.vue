8e122f8b4225e98e7421e7c49ab44dad027d87ae
Vale <leonvalenta1@gmail.com>
2025-01-15 10:48:00 +0100

    Poravak loga

--- frontend/quasar-project/src/pages/IndexPage.vue
+++ frontend/quasar-project/src/pages/IndexPage.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page class="flex flex-center">
-    <img alt="Logo" src="~assets/logo.jpg" />
+    <img alt="Logo" src="~assets/logo.jpg" style="padding: 1%; object-fit: contain; height: 80%; width: 80%;" />
   </q-page>
 </template>
 
