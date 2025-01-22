0e48dbabdb0691504241ad55c5e0baf7f066e230
VaLe202017 <leonvalenta1@gmail.com>
2025-01-22 14:23:28 +0100

    Maknuto nepotrebno iz apija

--- backend/index.cjs
+++ backend/index.cjs
@@ -33,28 +33,7 @@ connection.connect((err) => {
   console.log("Connected to MySQL database!");
 });
 
-app.post("/api/artikli", (req, res) => {
-  const { sifra_artikla, naziv_artikla, dostupna_kolicina, cijena_dan } =
-    req.body;
-  const sql =
-    "INSERT INTO artikli (sifra_artikla, naziv_artikla, dostupna_kolicina, cijena_dan) VALUES (?, ?, ?, ?)";
-  const values = [sifra_artikla, naziv_artikla, dostupna_kolicina, cijena_dan];
-
-  connection.query(sql, values, (err, result) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-    res.status(201).json({
-      id: result.insertId,
-      sifra_artikla,
-      naziv_artikla,
-      dostupna_kolicina,
-      cijena_dan,
-    });
-  });
-});
-
+//prikaz opreme
 app.get("/api/artikli", (req, res) => {
   const sql = "SELECT * FROM artikli";
   connection.query(sql, (err, results) => {
@@ -66,6 +45,7 @@ app.get("/api/artikli", (req, res) => {
   });
 });
 
+//registracija
 app.post("/api/korisnici", (req, res) => {
   const {
     sifra_korisnika,
@@ -100,242 +80,7 @@ app.post("/api/korisnici", (req, res) => {
   });
 });
 
-app.get("/api/korisnici", (req, res) => {
-  const sql = "SELECT * FROM korisnici";
-  connection.query(sql, (err, results) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-    res.status(200).json(results);
-  });
-});
-
-app.post("/api/narudzbe", (req, res) => {
-  const {
-    sifra_narudzbe,
-    sifra_korisnika,
-    datum_iznajmljivanja,
-    datum_vracanja,
-    ukupan_iznos,
-  } = req.body;
-  const sql =
-    "INSERT INTO narudzbe (sifra_narudzbe, sifra_korisnika, datum_iznajmljivanja, datum_vracanja, ukupan_iznos) VALUES (?, ?, ?, ?, ?)";
-  const values = [
-    sifra_narudzbe,
-    sifra_korisnika,
-    datum_iznajmljivanja,
-    datum_vracanja,
-    ukupan_iznos,
-  ];
-
-  connection.query(sql, values, (err, result) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-    res.status(201).json({
-      id: result.insertId,
-      sifra_narudzbe,
-      sifra_korisnika,
-      datum_iznajmljivanja,
-      datum_vracanja,
-      ukupan_iznos,
-    });
-  });
-});
-
-app.get("/api/narudzbe", (req, res) => {
-  const sql = "SELECT * FROM narudzbe";
-  connection.query(sql, (err, results) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-    res.status(200).json(results);
-  });
-});
-
-app.post("/api/stavke_narudzbe", (req, res) => {
-  const { sifra_artikla_narudzbe, sifra_narudzbe, sifra_artikla } = req.body;
-  const sql =
-    "INSERT INTO stavke_narudzbe (sifra_artikla_narudzbe, sifra_narudzbe, sifra_artikla) VALUES (?, ?, ?)";
-  const values = [sifra_artikla_narudzbe, sifra_narudzbe, sifra_artikla];
-
-  connection.query(sql, values, (err, result) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-    res.status(201).json({
-      id: result.insertId,
-      sifra_artikla_narudzbe,
-      sifra_narudzbe,
-      sifra_artikla,
-    });
-  });
-});
-
-app.get("/api/stavke_narudzbe", (req, res) => {
-  const sql = "SELECT * FROM stavke_narudzbe";
-  connection.query(sql, (err, results) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-    res.status(200).json(results);
-  });
-});
-
-app.put("/api/artikli/:id", (req, res) => {
-  const { id } = req.params;
-  const { sifra_artikla, naziv_artikla, dostupna_kolicina, cijena_dan } =
-    req.body;
-  const sql =
-    "UPDATE artikli SET sifra_artikla = ?, naziv_artikla = ?, dostupna_kolicina = ?, cijena_dan = ? WHERE sifra_artikla = ?";
-  const values = [
-    sifra_artikla,
-    naziv_artikla,
-    dostupna_kolicina,
-    cijena_dan,
-    id,
-  ];
-
-  connection.query(sql, values, (err) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-    res.status(200).json({ message: "Artikl updated successfully" });
-  });
-});
-
-app.put("/api/korisnici/:id", (req, res) => {
-  const { id } = req.params;
-  const {
-    sifra_korisnika,
-    ime_korisnika,
-    prezime_korisnika,
-    broj_telefona_korisnika,
-    email_korisnika,
-  } = req.body;
-  const sql =
-    "UPDATE korisnici SET sifra_korisnika = ?, ime_korisnika = ?, prezime_korisnika = ?, broj_telefona_korisnika = ?, email_korisnika = ? WHERE sifra_korisnika = ?";
-  const values = [
-    sifra_korisnika,
-    ime_korisnika,
-    prezime_korisnika,
-    broj_telefona_korisnika,
-    email_korisnika,
-    id,
-  ];
-
-  connection.query(sql, values, (err) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-    res.status(200).json({ message: "Korisnik updated successfully" });
-  });
-});
-
-app.put("/api/narudzbe/:id", (req, res) => {
-  const { id } = req.params;
-  const {
-    sifra_narudzbe,
-    sifra_korisnika,
-    datum_iznajmljivanja,
-    datum_vracanja,
-    ukupan_iznos,
-  } = req.body;
-  const sql =
-    "UPDATE narudzbe SET sifra_narudzbe = ?, sifra_korisnika = ?, datum_iznajmljivanja = ?, datum_vracanja = ?, ukupan_iznos = ? WHERE sifra_narudzbe = ?";
-  const values = [
-    sifra_narudzbe,
-    sifra_korisnika,
-    datum_iznajmljivanja,
-    datum_vracanja,
-    ukupan_iznos,
-    id,
-  ];
-
-  connection.query(sql, values, (err) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-    res.status(200).json({ message: "Narudzba updated successfully" });
-  });
-});
-
-app.put("/api/stavke_narudzbe/:id", (req, res) => {
-  const { id } = req.params;
-  const { sifra_artikla_narudzbe, sifra_narudzbe, sifra_artikla } = req.body;
-  const sql =
-    "UPDATE stavke_narudzbe SET sifra_artikla_narudzbe = ?, sifra_narudzbe = ?, sifra_artikla = ? WHERE sifra_artikla_narudzbe = ?";
-  const values = [sifra_artikla_narudzbe, sifra_narudzbe, sifra_artikla, id];
-
-  connection.query(sql, values, (err) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-    res.status(200).json({ message: "Stavka narudzbe updated successfully" });
-  });
-});
-
-app.delete("/api/artikli/:id", (req, res) => {
-  const { id } = req.params;
-  const sql = "DELETE FROM artikli WHERE sifra_artikla = ?";
-
-  connection.query(sql, [id], (err) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-    res.status(200).json({ message: "Artikl deleted successfully" });
-  });
-});
-
-app.delete("/api/korisnici/:id", (req, res) => {
-  const { id } = req.params;
-  const sql = "DELETE FROM korisnici WHERE sifra_korisnika = ?";
-
-  connection.query(sql, [id], (err) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-    res.status(200).json({ message: "Korisnik deleted successfully" });
-  });
-});
-
-app.delete("/api/narudzbe/:id", (req, res) => {
-  const { id } = req.params;
-  const sql = "DELETE FROM narudzbe WHERE sifra_narudzbe = ?";
-
-  connection.query(sql, [id], (err) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-    res.status(200).json({ message: "Narudzba deleted successfully" });
-  });
-});
-
-app.delete("/api/stavke_narudzbe/:id", (req, res) => {
-  const { id } = req.params;
-  const sql = "DELETE FROM stavke_narudzbe WHERE sifra_artikla_narudzbe = ?";
-
-  connection.query(sql, [id], (err) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-    res.status(200).json({ message: "Stavka narudzbe deleted successfully" });
-  });
-});
-
+//prikaz terena
 app.get("/api/tereni", (req, res) => {
   const sql = "SELECT * FROM Tereni";
   connection.query(sql, (err, results) => {
@@ -347,6 +92,7 @@ app.get("/api/tereni", (req, res) => {
   });
 });
 
+//dodavanje terena
 app.post("/api/tereniAdd", (req, res) => {
   const { Naziv, Lokacija, Radno_vrijeme } = req.body;
   const sql =
@@ -362,6 +108,7 @@ app.post("/api/tereniAdd", (req, res) => {
   });
 });
 
+//admin login
 app.post("/api/admin", async (req, res) => {
   const { username, password } = req.body;
 
@@ -387,22 +134,4 @@ app.post("/api/admin", async (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-});
-
-// složeni upit
-app.get("/api/artikli-with-korisnici", (req, res) => {
-  const sql = `
-    SELECT a.*, k.ime_korisnika, k.prezime_korisnika
-    FROM artikli a
-    INNER JOIN stavke_narudzbe sn ON a.sifra_artikla = sn.sifra_artikla
-    INNER JOIN narudzbe n ON sn.sifra_narudzbe = n.sifra_narudzbe
-    INNER JOIN korisnici k ON n.sifra_korisnika = k.sifra_korisnika
-  `;
-  connection.query(sql, (err, results) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-    res.status(200).json(results);
-  });
 });
